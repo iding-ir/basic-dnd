6d982da0eb1e358fdbb590d99cb74feaef84c020
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2022-10-26 20:25:15 +0200

    Test and bugfix for long press

--- src/components/ItemView/ItemView.tsx
+++ src/components/ItemView/ItemView.tsx
@@ -34,8 +34,6 @@ export const ItemView = ({
     document.body.removeEventListener("pointermove", bodyPointerMove);
     document.body.removeEventListener("pointerup", bodyPointerUp);
 
-    // 13.
-    // Remove event listener when we want to allow scrolling in touch devices again
     itemsViewRef.current?.removeEventListener("touchmove", preventDefault);
 
     setDragItem(undefined);
@@ -45,13 +43,8 @@ export const ItemView = ({
   const cancelLongPress = () => {
     console.log("cancelLongPress");
 
-    // 16.
-    // User has moved, or released pointer too early, or their device has changed orientation.
-    // We kill the long press timeout.
     longPressTimer && clearTimeout(longPressTimer);
 
-    // 17.
-    // Terminate event listener garbage
     elementRef.current?.removeEventListener("pointerup", cancelLongPress);
     elementRef.current?.removeEventListener("pointermove", cancelLongPress);
     elementRef.current?.removeEventListener("pointercancel", cancelLongPress);
@@ -60,51 +53,35 @@ export const ItemView = ({
   const addLongPressCancelers = () => {
     console.log("addLongPressCancelers");
 
-    // 15.
-    // Three events can cancel a long press: up, move, and cancel
-    // We add them to our element so we can clear the long press timeout if one of them happens.
     elementRef.current?.addEventListener("pointerup", cancelLongPress);
     elementRef.current?.addEventListener("pointermove", cancelLongPress);
     elementRef.current?.addEventListener("pointercancel", cancelLongPress);
   };
 
-  const waitForLongPress = () => {
+  const waitForLongPress = (event: React.PointerEvent<HTMLDivElement>) => {
     addLongPressCancelers();
 
     longPressTimer = setTimeout(() => {
       console.log("User has long pressed!");
 
-      // 18.
-      // Only add certain event listeners after long press
       document.body.addEventListener("pointermove", bodyPointerMove);
       document.body.addEventListener("pointerup", bodyPointerUp);
+
+      // 19.
+      // We also need to set DragItem after long press is triggered.
+      // But we don't 'event' here, so I passed it as a parameter.
+      const coords: Coords = { x: event.clientX, y: event.clientY };
+      setDragItem(item);
+      setDragItemCoords(coords);
     }, 1000);
   };
 
   const onPointerDown = (event: React.PointerEvent<HTMLDivElement>) => {
     console.log("onPointerDown", item.name);
 
-    // 12.
-    // Only prevent default when pointerdown is triggered
-    // It's interesting that what should be prevented is 'touchmove', not 'pointermove'
     itemsViewRef.current?.addEventListener("touchmove", preventDefault);
 
-    // Tested it on:
-    // Chrome desktop: works fine
-    // Chrome desktop touch simulation: works fine
-    // Chrome on android: dragging works fine, but now I cannot scroll.
-    // Makes sense. Need to prevent default only when dragging is in progress
-
-    // 14.
-    // Cool, but now the question is: how do we scroll in touch devices?
-    // Because when you think about it, touch to scroll can always be confused with touch to drag-and-drop.
-    // Solution: use long press to activate drag-and-drop in touch devices
-    const coords: Coords = { x: event.clientX, y: event.clientY };
-
-    waitForLongPress();
-
-    setDragItem(item);
-    setDragItemCoords(coords);
+    waitForLongPress(event);
   };
 
   const onPointerMove = (event: React.PointerEvent<HTMLDivElement>) => {
